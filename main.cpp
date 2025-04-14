92ebc2ba1b52c13299511d3bc2ed988342d9069e
lilys-corner <lillianyu0501@gmail.com>
2025-04-13 21:58:12 -0400

    Uploading Project

--- main.cpp
+++ main.cpp
@@ -7,11 +7,31 @@ using namespace std;
         Can turn this into .txt if it won't accept .csv
         Two weights for each length, Distance and Cost
 2:
-    Find shortest path from source to destination
-
-
-
-
+    Find shortest path between origin airport and destination airport
+        Provide message if no path
+3:
+    Find all shortest paths between origin airports and all airports in the destination state
+        Provide message if no path
+4:
+    Find shortest path between origin airport and destination with stops
+        Provide message if no path
+        3 stops = Source -> stop 1 -> stop 2 -> stop 3 -> Destination
+5:
+    Direct flight connections
+        Total = inbound + outbound
+6:
+    Create undirected graph G_u from G
+        Only one directed edge (between u and v) case:
+            Ignore Distance, Cost is an undirected weight edge
+        Two directed edges (between u and v) case:
+            Keep edge with minimum Cost value
+            Ignore distance, Cost is an undirected weight edge
+7:
+    Create a Minimal Spanning Tree (MST) w/ Prim's algorithm on G_u
+        Output content of MST and total cost (see sample output)
+8:
+    Create a Minimal Spanning Tree (MST) w/ Kruskal's algorithm on G_u
+        Output content of MST and total cost (see sample output)
 
 */
 
