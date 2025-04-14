7b8a852c058e028e253f529bd9659a8ca4cd1d66
lilys-corner <lillianyu0501@gmail.com>
2025-04-13 21:39:30 -0400

    Changes by subtl

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 using namespace std;
 
+/*
+1:
+    Create weighted directed graph G from the provided .csv file
+        Can turn this into .txt if it won't accept .csv
+        Two weights for each length, Distance and Cost
+2:
+    Find shortest path from source to destination
 
+
+
+
+
+*/
 
 
 
 //HEWWOO
+// i like how we don't know what we're doing
 
 // B)
 int main() {
