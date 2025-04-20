b345f6e3f70cc224e3aa397d31282c5816de6e14
lilys-corner <164098509+lilys-corner@users.noreply.github.com>
2025-04-19 20:49:45 -0400

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -9,17 +9,24 @@ class Graph {
 public:
     Graph() {}
     Graph(const Graph& other);
+    void fix(const Graph& other);
 
     void insert_vertex(const Vertex<T>& ver);
     void add_edge(const Vertex<T>& ver1, const Vertex<T>& ver2, int weight, int cost); //connect ver1 with ver2
 
     Graph<T> primMST(); //creates a MST using Prim's algorithm given an unsorted GU
+    Graph<T> kruskalMST(); //create a MST using kruskal's algorithm
+
 
     void print() const;
 
     void DFS(Vertex<T>& ver);
     int dijkstra_shortest_path(const Vertex<T>& src, const Vertex<T>& dest, const std::vector<Vertex<T>>& state_airport, const std::string& stateName, const int& print_mode);
 
+    void shortest_path_stops(const Vertex<T>& src, const Vertex<T>& dest, int stops);
+
+
+    void count_direct_flights();
     int if_exists(const Vertex<T>& ver);
     Vertex<T> airport_to_vector(const T& airportName);
     std::vector<Vertex<T>> state_to_vector(const T& stateName);
