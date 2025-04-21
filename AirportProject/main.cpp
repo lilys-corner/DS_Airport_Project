c93597db488e792e89636013031e894cfb2445a5
lilys-corner <164098509+lilys-corner@users.noreply.github.com>
2025-04-20 21:05:04 -0400

    Update main.cpp

--- AirportProject/main.cpp
+++ AirportProject/main.cpp
@@ -8,65 +8,6 @@ using namespace std;
 // Open in.txt file to read from
 ifstream infile("airports.txt");
 
-
-/*
-1 (HAILEY):
-    Create weighted directed graph G from the provided .csv file
-        Can turn this into .txt if it won't accept .csv
-        Two weights for each length, Distance and Cost
-DONE!
-
-2 (LILY):
-    Find shortest path between origin airport and destination airport
-        Provide message if no path
-DONE!
-
-3 (LILY):
-    Find all shortest paths between a given origin airport and all airports in the destination state
-        Provide message if no path
-DONE!
-
-4 (HAILEY):
-    Find shortest path between origin airport and destination with stops
-        Provide message if no path
-        3 stops = Source -> stop 1 -> stop 2 -> stop 3 -> Destination
-
-5 (LILY):
-    Direct flight connections
-        Total = inbound + outbound
-        Direct meaning there is only one direct edge between them
-            Tampa -edge-> New York: Tampa has 1 outbound edge there, NY has one inbound
-DONE!
-
-6 (HAILEY):
-    Create undirected graph G_u from G
-        Only one directed edge (between u and v) case:
-            Ignore Distance, Cost is an undirected weight edge
-        Two directed edges (between u and v) case:
-            Keep edge with minimum Cost value
-            Ignore distance, Cost is an undirected weight edge
-
-7 (HAYLEY):
-    Create a Minimal Spanning Tree (MST) w/ Prim's algorithm on G_u
-        Output content of MST and total cost (see sample output)
-        If G_u is disconnected, provide message it cannot be formed
-
-    //the basics of the algorithm
-Done!
-
-8 (HAYLEY):
-    Create a Minimal Spanning Tree (MST) w/ Kruskal's algorithm on G_u
-        Output content of MST and total cost (see sample output)
-        If G_u is disconnected, algorithm provides minimum spanning forest
-        consisting of a minimum spanning tree for each connected component
-
-    //the basics of the algorithm cause idk what i'm doing
-        greedy approach - each iteration it finds an edge which has least
-        weight and adds it ot the growing spanning tree
-Done!
-
-*/
-
 int main() {
     if (infile.is_open() == false) {
         cout << "Error: Unable to open airports.txt" << endl;
